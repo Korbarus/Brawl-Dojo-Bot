d3b886f608741a6c15473d47b4ba78ae5adb05be
Korbarus <korbarus2951@gmail.com>
2019-08-18 22:08:26 +0100

    Testing of Embed Messages
    Discord makes use of RichPresence, which we can use to make our general messages look professional and unique. As a result of this, I've been tinkering with simple embed messages (see the help command).
    
    Additionally, the mentors file is where all of the mentor related commands will be stored as functions.

--- main.py
+++ main.py
@@ -8,15 +8,34 @@ prefix = "="
 
 
 @client.event
+
 async def on_message(message):
+    print(message.channel.name+"-"+message.author.name+":"+message.content)
+    channel = message.channel
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
-    print(message.channel.name+"-"+message.author.name+":"+message.content)
+
+    if message.content == ("{0}".format(prefix)):
+        #finds the channel to send it back to
+        msg = ("That's the prefix for this bot. Type '{0}help' for current commands!".format(prefix)) #msg can't handle more than 1 argument
+        await channel.send(msg) #sends the message
+
+    #Example command - checks if the message has "=hi" in it.
     if message.content.startswith("{0}hi".lower().format(prefix)):
-        channel = message.channel
-        msg = ("Hi, "+message.author.mention+"!")
-        await channel.send(msg)
+        #finds the channel to send it back to
+        msg = ("Hi, "+message.author.mention+"!") #msg can't handle more than 1 argument
+        await channel.send(msg) #sends the message
+
+    if message.content.startswith("{0}help".lower().format(prefix)):
+
+        embed = discord.Embed(title="**Mentor Listing**", color=0x00ff00)
+        embed.set_thumbnail(url=client.user.avatar_url)
+        embed.add_field(name="{0}list mentor axe".format(prefix), value="*Lists the top 10 mentors for axe globally.*", inline=True)
+        embed.add_field(name="{0}list mentor axe eu".format(prefix), value="*Lists the top 10 mentors for axe in the EU.*", inline=True)
+        embed.add_field(name="{0}list mentor axe na".format(prefix), value="*Lists the top 10 mentors for axe in the US.*", inline=True)
+        embed.set_footer(text="This bot was made by @Korbarus1384! please buy me food i haven't eaten in weeks this is a cry for help this bot is my only chance of escape")
+        await channel.send(embed=embed)
 
 @client.event
 async def on_ready():
